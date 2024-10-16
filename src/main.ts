d9426b8dcce50a0486e83320cebf5f00e26df0af
tickyyyy <102706661+tickyyyy@users.noreply.github.com>
2024-10-16 16:51:01 -0700

    Step 1 Complete: Added canvas with styling

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
 import "./style.css";
 
-const APP_NAME = "Leif's Game";
+const APP_NAME = "LT Paint";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// Canvas
+const canvas = document.createElement("canvas");
+const canvasWidth = 256;
+const canvasHeight = 256
+canvas.id = "canvas";
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+app.appendChild(canvas);
+
+// Get the 2D context
+const ctx = canvas.getContext("2d");
+if (ctx){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+
+// Title
+const titleElement = document.createElement("h1");
+titleElement.textContent = APP_NAME;
+titleElement.style.marginBottom = "20px";
+app.prepend(titleElement); // prepend to keep on top
